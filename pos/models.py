848a8361a7a40e926f104d035a2ef0bc16c6471d
Stef Pletinck <stef.plet@gmail.com>
2016-05-24 14:33:33 +0200

    removed user object. Will be using django users

--- pos/models.py
+++ pos/models.py
@@ -13,12 +13,12 @@ class Product(models.Model):
 
 
 
-class User(models.Model):
-    user_id = models.AutoField(primary_key = True)
-    user_name = models.CharField(max_length=50)
-    user_isAdmin = models.BooleanField()
-
-    def __str__(self):
-        return user_name
-
+#class User(models.Model):
+#    user_id = models.AutoField(primary_key = True)
+#    user_name = models.CharField(max_length=50)
+#    user_isAdmin = models.BooleanField()
+#
+#    def __str__(self):
+#        return user_name
+#
 
