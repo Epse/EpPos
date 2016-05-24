23a807f8d3507f232a31cf76d0e9b1047202607a
Stef Pletinck <stef.plet@gmail.com>
2016-05-24 14:13:40 +0200

    added user and product models

--- pos/models.py
+++ pos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    product_price = models.DecimalField(max_digits=7,decimal_places=3)
+    product_stockApplies = models.BooleanField()
+    product_stock = models.PositiveSmallIntegerField()
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key = True)
+    user_name = models.CharField(max_length=50)
+    user_isAdmin = models.BooleanField()
+
+
