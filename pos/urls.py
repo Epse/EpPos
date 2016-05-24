226ed74a93ddcf932a9c9db30dd0190010984b90
Stef Pletinck <stef.plet@gmail.com>
2016-05-24 15:04:49 +0200

    added order and addition view

--- pos/urls.py
+++ pos/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^order/$', views.order, name='order'),
+    url(r'^addition/$', views.addition, name='addition'),
 ]
--- pos/views.py
+++ pos/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello World. This is the pos starting page")
 
+def order(request):
+    return HttpResponse("Hello. Here you would be able to take an order")
+
+def addition(request):
+    return HttpResponse("Hello. Here you will be able to see the order, as well as the due amount")
