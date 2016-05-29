95ca69cf55a119004351333867633b0981ed21b6
Stef Pletinck <stef.plet@gmail.com>
2016-05-29 11:17:32 +0200

    made everything redirect to /pos/order

--- pos/urls.py
+++ pos/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', RedirectView.as_view(pattern_name='order')),
     url(r'^order/$', views.order, name='order'),
     url(r'^addition/(?P<operation>[A-Za-z0-9]*)/?$', views.addition, name='addition'),
 ]
