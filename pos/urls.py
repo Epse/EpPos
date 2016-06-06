6aeb3ee358a91c1e5c08763f254230d1306477ce
Epse <stef.plet@gmail.com>
2016-06-06 20:10:37 +0200

    fixed #12

--- pos/urls.py
+++ pos/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='order')),
     url(r'^order/$', views.order, name='order'),
-    url(r'^addition/(?P<operation>[A-Za-z0-9]*)/?$', views.addition, name='addition'),
+    url(r'^addition/(?P<operation>[A-Za-z0-9\.\+\(\)\-\_]*)/?$', views.addition, name='addition'),
 ]
