393382c9377627408ea1b6ec582749fadca2f859
Sebastian Spaeth <Sebastian@SSpaeth.de>
2017-10-22 01:24:04 +0200

    Huge messy refactor
    This is refactoring without backward compat. Won't be able to migrate
    existing dbs (for now). (extra effort)
    
    We can now delete an item at the correct position (and we do not just filter
    according to its name). This required us to have an extra intermediary table
    in an django M2M relation that saves each product-order relation.
    
    Also, we do not pass around weird json when adding and removing things. Adding
    happens by passing a product.id and removing by passing a "ProductInOrder" id.
    
    The new removal API has the advantage that we can remove a product for
    multiple open orders as one of the goals was to have multiple orders open
    simultanously.
    
    Everything should still work

--- pos/urls.py
+++ pos/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='order')),
     url(r'^order/$', views.order, name='order'),
     url(r'^addition/(?P<operation>[A-Za-z0-9\.\+\(\)\-\_ ]*)/?$', views.addition, name='addition'),
+    url(r'^order/rem/pio/$', views.remove_from_order,
+        name='rem_from_order'),
+    url(r'^order(?P<order_id>[0-9]+)/rem/pio(?P<pio_id>[0-9]*)/$',
+        views.remove_from_order, name='rem_from_order'),
     url(r'^cash/(?P<amount>[0-9\.]*)/?$', views.cash, name='cash')
 ]
