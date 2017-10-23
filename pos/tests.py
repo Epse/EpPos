6b3cb08fdaee4712b3a7c6b874d2749776cc44a2
Stef Pletinck <s@stefpletinck.be>
2017-10-23 14:03:02 +0200

    This shouldn't use the database

--- pos/tests.py
+++ pos/tests.py
@@ -1,15 +1,15 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 from decimal import *
 from .helper import parse_json_product_list, product_list_to_json
 from .models import Product
 
-class HelperTestCase(TestCase):
+class HelperTestCase(SimpleTestCase):
     product_list = []
     def setUp(self):
-        self.product_list.append(Product.objects.create(product_stockApplies = False, product_name="one", product_price=Decimal(12)))
-        self.product_list.append(Product.objects.create(product_stockApplies = False, product_name="two", product_price=Decimal(4)))
-        self.product_list.append(Product.objects.create(product_stockApplies = False, product_name="three", product_price=Decimal(0)))
-        self.product_list.append(Product.objects.create(product_stockApplies = False, product_name="four", product_price=Decimal(5000)))
+        self.product_list.append(Product(product_stockApplies = False, product_name="one", product_price=Decimal(12)))
+        self.product_list.append(Product(product_stockApplies = False, product_name="two", product_price=Decimal(4)))
+        self.product_list.append(Product(product_stockApplies = False, product_name="three", product_price=Decimal(0)))
+        self.product_list.append(Product(product_stockApplies = False, product_name="four", product_price=Decimal(5000)))
 
     def test_json_parsing(self):
         json = product_list_to_json(self.product_list)
