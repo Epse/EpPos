0fc4bdfae58e089f60ee47d20e60b21f96ca79a4
Stef Pletinck <s@stefpletinck.be>
2017-10-24 13:23:29 +0200

    Test for product name validation

--- pos/tests.py
+++ pos/tests.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.test import SimpleTestCase, Client, TransactionTestCase
 from django.core.urlresolvers import reverse
 from decimal import *
 from .helper import parse_json_product_list, product_list_to_json
-from .models import Product, Cash
+from .models import Product, Cash, validate_product_name
 
 class HelperTestCase(SimpleTestCase):
     product_list = []
@@ -24,9 +25,13 @@ class HelperTestCase(SimpleTestCase):
             self.assertEqual(test_list[i].product_name, self.product_list[i].product_name)
 
 class CashViewTestCase(TransactionTestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('test', 'test@example.com', 'pswd')
+        self.client = Client()
+
     def test_cash_set(self):
-        client = Client()
-        response = client.get(reverse('cash', args=[5]))
+        self.client.login(username='test', password='pswd')
+        response = self.client.get(reverse('cash', args=[5]))
         self.assertEqual(response.status_code, 200)
 
         cash, _ = Cash.objects.get_or_create(id=0)
@@ -37,12 +42,17 @@ class CashViewTestCase(TransactionTestCase):
         cash.cash_amount = 5
         cash.save()
 
-        client = Client()
-        response = client.get(reverse('cash', args=[0]))
+        self.client.login(username='test', password='pswd')
+        response = self.client.get(reverse('cash', args=[0]))
         self.assertEqual(response.status_code, 200)
 
         cash, _ = Cash.objects.get_or_create(id=0)
         self.assertEqual(cash.cash_amount, 0)
+
+    def test_not_logged_in(self):
+        self.client.logout()
+        response = self.client.get(reverse('cash', args=[0]))
+        self.assertEqual(response.status_code, 403)
 
 class OrderViewTestCase(TransactionTestCase):
     product_list = []
@@ -69,3 +79,28 @@ class OrderViewTestCase(TransactionTestCase):
         for i in range(0, len(response_product_list)):
             self.assertEqual(response_product_list[i].product_name, self.product_list[i].product_name)
             self.assertEqual(response_product_list[i].product_price, self.product_list[i].product_price)
+
+
+class ProductNameTestCase(SimpleTestCase):
+    test_names_correct = []
+    test_names_incorrect = []
+    def setUp(self):
+        self.test_names_correct.append('ProductOne')
+        self.test_names_correct.append('Product Two')
+        self.test_names_correct.append('3')
+        self.test_names_correct.append('prodUct_four')
+
+        self.test_names_incorrect.append(' ')
+        self.test_names_incorrect.append('')
+        self.test_names_incorrect.append(' One_2 3')
+        self.test_names_incorrect.append('product-one')
+
+    def test_correct_names(self):
+        for name in self.test_names_correct:
+            # This might seem dodgy, but if it raises an error,
+            # that will count as a failed test.
+            validate_product_name(name)
+
+    def test_incorrect_names(self):
+        for name in self.test_names_incorrect:
+            self.assertRaises(ValidationError, validate_product_name, name)
