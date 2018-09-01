afdbac2e74b277311bef2776bd1101f0e44ee3c8
Stef Pletinck <s@stefpletinck.be>
2018-09-01 13:56:52 +0200

    working docker. View at port 8001

---
+++ manage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function up() {
+    docker-compose up -d
+}
+
+function setup() {
+    docker-compose up -d --build && docker-compose exec web ./manage.py createsuperuser
+}
+
+function stop() {
+    docker-compose stop
+}
+
+function clean() {
+    docker-compose down
+    docker-compose rm
+}
+
+function redo() {
+    clean
+    setup
+}
+
+$@
