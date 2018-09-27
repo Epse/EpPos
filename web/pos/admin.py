ed1036afa62ab9804ce9b4b047527af9096b32da
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-09-27 23:30:21 +0500

    Added Custom LowStockFilter in ProductAdmin display_list view

--- web/pos/admin.py
+++ web/pos/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import Product, Cash, Order, Setting
-
+from django.contrib.admin import SimpleListFilter
+from django.db.models import F
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(Cash)
-admin.site.register(Order)
 
 # Admin settings
 admin.site.site_header = "EpPos Administration"
@@ -22,3 +21,96 @@ class SettingAdmin(admin.ModelAdmin):
     # Deleting a setting seems odd...
     def has_delete_permission(self, *args, **kwargs):
         return False
+
+    # List Display Page Configuration
+    list_display = (
+        'key',
+        'value'
+    )
+
+
+# Custom Filter for ProductAdmin
+class LowStockFilter(admin.SimpleListFilter):
+
+    title = 'Stock Available'
+    parameter_name = 'stock_available'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('low', 'Low Stock'),
+            ('high', 'High Stock')
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value() == 'low':
+            return queryset.filter(stock__lte=F('minimum_stock'))
+        if self.value() == 'high':
+            return queryset.filter(stock__gte=F('minimum_stock'))
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    The Admin Configuration for the model Product.
+    """
+
+    # List Page Display Configuration
+    list_display = (
+        'code',
+        'name',
+        'price',
+        'stock',
+        'minimum_stock',
+        'stock_applies'
+    )
+
+    list_display_links = (
+        'code',
+        'name',
+    )
+
+    # Search Configuration
+    search_fields = (
+        'code',
+        'name'
+    )
+
+    # List filter
+    list_filter = (
+        LowStockFilter,
+    )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """"
+    Admin Site Configuration for Order Model
+    """
+
+    # List page display configuration
+    list_display = (
+        'user',
+        'total_price',
+        'done',
+        'last_change'
+    )
+
+    # Sidebar Filter Configuration
+    list_filter = (
+        'done',
+    )
+
+    # Order by not Done
+    ordering = ('done', )
