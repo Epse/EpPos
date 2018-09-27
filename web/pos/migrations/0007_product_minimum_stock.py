e20b8ba6d46fc74e1d2ff9d78f35b3d117d96183
Zero Cool <shahzaibakash@gmail.com>
2018-09-28 03:24:18 +0500

    Updated migration file
    Was causing error in the pull request #62

--- web/pos/migrations/0007_product_minimum_stock.py
+++ web/pos/migrations/0007_product_minimum_stock.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pos', '0005_auto_20180901_1626'),
+        ('pos', '0006_auto_20180926_1433'),
     ]
 
     operations = [
