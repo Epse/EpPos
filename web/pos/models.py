3076f90a63c43dbe6d27f0d82c79fbd16deccdea
Stef Pletinck <s@stefpletinck.be>
2018-10-01 13:59:46 +0200

    Fixed styling issues. Fixed #64

--- web/pos/models.py
+++ web/pos/models.py
@@ -41,7 +41,8 @@ class Product(models.Model):
     minimum_stock = models.PositiveSmallIntegerField(default=0)
     stock = models.IntegerField(default=0)
     code = models.CharField(max_length=50, unique=True, null=True, blank=True)
-    colour = models.CharField(max_length=10, choices=COLOUR_CHOICES, default="blue")
+    colour = models.CharField(max_length=10,
+                              choices=COLOUR_CHOICES, default="blue")
 
     def __str__(self):
         return self.name
